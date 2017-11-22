e7d32bdc8f6483bf20d296e34dec30c634258fe7
Milo Turner <iitalics@gmail.com>
2017-11-22 00:45:17 -0500

    refactored tests to be slightly less cryptic

--- test/test_main.ml
+++ test/test_main.ml
@@ -4,6 +4,17 @@ open Ast_405.Parsetree
 open Ast_405.Longident
 module Nano = Nanocaml
 
+let stri_type_decl stri =
+  match stri.pstr_desc with
+  | Pstr_type (_, [td]) -> td
+  | _ -> raise (Failure "incorrect structure item accessor")
+
+let stri_mod_bind stri =
+  match stri.pstr_desc with
+  | Pstr_module mb -> mb
+  | _ -> raise (Failure "incorrect structure item accessor")
+
+
 let t_parsing =
   "parsing" >:::
     let open Nanocaml.Lang in
@@ -40,67 +51,64 @@ let t_parsing =
 
       "nonterm_of_type_decl(1)" >::
         begin fun _ ->
-        let si = [%stri type expr = [ `Var of string | `Int of int ] ] in
-        match si.pstr_desc with
-        | Pstr_type (_, [td]) ->
-           begin match nt_of_td td with
-           | {npnt_name = "expr";
-              npnt_productions = [
-                  {npp_name = "Var"};
-                  {npp_name = "Int"} ]} -> ()
-           | _ ->
-              assert_failure "expr nonterm has the wrong structure"
-           end
-        | _ -> assert_failure "unreachable"
+        match stri_type_decl
+                [%stri type expr =
+                   [ `Var of string
+                   | `Int of int ] ]
+              |> nt_of_td
+        with
+        | {npnt_name = "expr";
+           npnt_productions = [
+               {npp_name = "Var"};
+               {npp_name = "Int"} ]} -> ()
+        | _ ->
+           assert_failure "expr nonterm has the wrong structure"
         end;
 
       "nonterm_of_type_decl(2)" >::
         begin fun _ ->
-        let si = [%stri type expr = Var of string | Int of int ] in
-        match si.pstr_desc with
-        | Pstr_type (_, [td]) ->
-           (try let _ = nt_of_td td in
-                assert_failure "expected nonterm to fail"
-            with Location.Error _ -> ());
-        | _ -> assert_failure "unreachable"
+        try
+          stri_type_decl
+            [%stri type expr =
+               Var of string
+             | Int of int ]
+          |> nt_of_td
+          |> ignore;
+          assert_failure "expected nonterm to fail"
+        with Location.Error _ -> ()
         end;
 
       "language_of_module(1)" >::
         begin fun _ ->
-        let si = [%stri module L0 = struct
-                    type a = [ `A of b ]
-                    and b = [ `B of a ]
-                  end]
+        let lang =
+          stri_mod_bind
+            [%stri module L0 = struct
+               type a = [ `A of b ]
+               and b = [ `B of a ]
+             end]
+          |> l_of_mb
         in
-        match si.pstr_desc with
-        | Pstr_module mb ->
-           let lang = l_of_mb mb in
-           assert_equal "L0" lang.npl_name;
-           assert_equal ["a"; "b"] (List.map (fun nt -> nt.npnt_name)
-                                      lang.npl_nonterms);
-           assert_equal [ {npp_name = "A"; npp_args = [ NPtype_nonterm "b" ]};
-                          {npp_name = "B"; npp_args = [ NPtype_nonterm "a" ]} ]
-             (lang.npl_nonterms
-              |> List.map (fun nt -> nt.npnt_productions)
-              |> List.concat)
-        | _ ->
-           assert_failure "unreachable"
+        assert_equal "L0" lang.npl_name;
+        assert_equal ["a"; "b"] (List.map (fun nt -> nt.npnt_name) lang.npl_nonterms);
+        assert_equal [ {npp_name = "A"; npp_args = [ NPtype_nonterm "b" ]};
+                       {npp_name = "B"; npp_args = [ NPtype_nonterm "a" ]} ]
+          (lang.npl_nonterms
+           |> List.map (fun nt -> nt.npnt_productions)
+           |> List.concat)
         end;
 
       "language_of_module(2)" >::
         begin fun _ ->
-        let si = [%stri module L0 = struct
-                    type a = [ `A of b ]
-                    type b = [ `B of a ] (* must be single recursive declaration *)
-                  end ]
-        in
-        match si.pstr_desc with
-        | Pstr_module mb ->
-           (try let _ = l_of_mb mb in
-                assert_failure "expected language to fail"
-            with Location.Error _ -> ())
-        | _ ->
-           assert_failure "unreachable"
+        try
+          stri_mod_bind
+            [%stri module L0 = struct
+               type a = [ `A of b ]
+               type b = [ `B of a ] (* must be single recursive declaration *)
+             end ]
+          |> l_of_mb
+          |> ignore;
+          assert_failure "expected language to fail"
+        with Location.Error _ -> ()
         end;
 
     ]

058aba1041c1c7f7765e5c21048a5e248da8bace
Milo Turner <iitalics@gmail.com>
2017-12-08 02:56:47 -0500

    added entry to driver (unfinished)

--- src/driver.ml
+++ src/driver.ml
@@ -5,6 +5,7 @@ let rewriter _config _cookies =
   let fallback = Ast_mapper.default_mapper in
 
   let structure_item mapper = function
+    (* module%language Lx = ... *)
     | {pstr_loc = loc;
        pstr_desc =
          Pstr_extension
@@ -17,6 +18,23 @@ let rewriter _config _cookies =
        let mb' = Lang_codegen.gen_module_binding lang in
        {pstr_loc = loc;
         pstr_desc = Pstr_module mb'}
+
+    (* let[@pass ...] pass_x = ... *)
+    | {pstr_loc = loc;
+       pstr_desc =
+         Pstr_value
+           (recflag,
+            [ {pvb_attributes = [ {txt = "pass"}, _ ]} as vb ])}
+      ->
+       if recflag = Asttypes.Recursive then
+         Location.raise_errorf ~loc
+           "nanopass may not be declared recursive"
+       else
+         let pass = Pass.pass_of_value_binding vb in
+         (* TODO: typecheck the pass first *)
+         let vb' = Pass_codegen.gen_pass_vb pass in
+         {pstr_loc = loc;
+          pstr_desc = Pstr_value (Nonrecursive, [ vb' ])}
 
     | str -> fallback.structure_item mapper str
   in
